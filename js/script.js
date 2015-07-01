ba5bec131f38a1467800d2b732b547d3bef508bc
Ness Trujillo <1469ness.nt@gmail.com>
2015-07-01 13:43:52 -0700

    set up responsive nav

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var nav = document.getElementById('navLinks');
+
+var button = document.getElementById('navButton');
+(Function()[
+	var width = window.innerWidth;
+
+
+	])"docReady", window));
+
+
+if (width >= 768 ([
+	nav.classList.('add');
+	nav.classList.('hide');
+	])
+
+button.addEventListener('click', function(){
+		nav.classList.toggle('hide');
+		nav.classListtoggle('show');
+
+}) 
+window.addEventListener('resize', function(){
+var width = window.innerWidth;
+if(width <= 768){
+	nav.classList.add('hide');
+	nav.classList.add('show');
+	}else{
+
+	}
+}
+    nav.classList.add('hide');
+	nav.classList.remove('show');
